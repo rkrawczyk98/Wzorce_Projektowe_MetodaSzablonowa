faf1bb007e4552d62253c61c8c4d936ebe1ac610
Rafał Krawczyk <rafal.krawczyk@microsoft.wsei.edu.pl>
2023-01-07 10:57:48 +0100

    Add project files.

---
+++ Wzorce_Projektowe_MetodaSzablonowa/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+abstract class ZamowienieTemplatka
+{
+    protected static bool _czyGratis;
+
+    abstract public void DoKoszyk();
+    abstract public void DoDostawa();
+    abstract public void DoPlatnosc();
+    protected static void DodanieGratisu() => Console.WriteLine("Dodano gratis...");
+
+    public void PrzetwarzajZamowienie(bool czyGratis)
+    {
+        _czyGratis = czyGratis;
+        DoKoszyk();
+        DoPlatnosc();
+        DoDostawa();
+    }
+}
+
+internal class ZamowienieStacjonarne : ZamowienieTemplatka
+{
+    override public void DoKoszyk() => Console.WriteLine("Wybranie produktów...");
+
+    override public void DoPlatnosc()
+    {
+        Console.WriteLine("Płatność w kasie (karta/gotówka)...");
+        if (_czyGratis)
+            DodanieGratisu();
+    }
+
+    override public void DoDostawa() => Console.WriteLine("Wydanie produktów (odbiór osobisty)...");
+}
+
+class ZamowienieOnline : ZamowienieTemplatka
+{
+    override public void DoKoszyk()
+    {
+        Console.WriteLine("Kompletowanie zamówienia...");
+        Console.WriteLine("Ustawiono parametry wysyłki...");
+    }
+
+    override public void DoPlatnosc()
+    {
+        Console.WriteLine("Płatność...");
+        if (_czyGratis)
+            DodanieGratisu();
+    }
+
+    override public void DoDostawa() => Console.WriteLine("Wysyłka...");
+}
+
+
+class Program
+{
+    public static void Main(String[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        ZamowienieTemplatka zamowienieOnline = new ZamowienieOnline();
+        zamowienieOnline.przetwarzajZamowienie(true);
+
+        Console.WriteLine();
+
+        ZamowienieTemplatka zamowienieStacjonarne = new ZamowienieStacjonarne();
+        zamowienieStacjonarne.przetwarzajZamowienie(false);
+
+    }
+}
